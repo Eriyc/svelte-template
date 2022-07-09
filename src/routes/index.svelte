6d03081f3ac1a5d30bee64b580c1ff6baa81c82f
Eric Dahlgren <eric@dgren.dev>
2022-07-09 22:25:12 +0200

    supabase functionality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,29 @@
 <script lang="ts">
+	import { supabase } from '$lib/supabase-client';
+	import { user } from '$lib/session-store';
+
+	user.set(supabase.auth.user());
+
+	supabase.auth.onAuthStateChange((_, session) => {
+		user.set(session?.user || null);
+	});
+
+	let loading = false;
+	let email: string;
+
+	const handleLogin = async () => {
+		try {
+			loading = true;
+			const { error } = await supabase.auth.signIn({ email });
+			if (error) throw error;
+			alert('Check your email for the login link!');
+		} catch (error: any) {
+			alert(error.error_description || error.message);
+		} finally {
+			loading = false;
+		}
+	};
+
 	export let projectName = 'Project Template';
 </script>
 
@@ -13,4 +38,17 @@
 			>
 		</a>
 	</section>
+	{#if !$user}
+		<section>
+			<form on:submit|preventDefault={handleLogin}>
+				<input class="inputField" type="email" placeholder="Your email" bind:value={email} />
+				<input
+					type="submit"
+					class="rounded-sm bg-indigo-600 py-2 px-4 font-semibold text-white"
+					value={loading ? 'Loading' : 'Send magic link'}
+					disabled={loading}
+				/>
+			</form>
+		</section>
+	{/if}
 </main>
